3a9146ad55bd86c08d6211f8dbb054600053d429
OlehKulyk2710 <92692410+OlehKulyk2710@users.noreply.github.com>
2022-07-23 01:43:58 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,15 +4,15 @@ import { nanoid } from 'nanoid';
 import Container from './Container';
 import ContactForm from './Form';
 import ContactList from './ContactList';
-// import Filter from './Filter';
+import Filter from './Filter';
 
 export class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
     name: '',
@@ -65,7 +65,7 @@ export class App extends Component {
         <h1 className={style.titlePrimary}>Phonebook</h1>
         <ContactForm onSubmit={this.formSubmitHandler} />
         <h2 className={style.titleSecondary}>Contacts</h2>
-        {/* <Filter filter={this.state.filter} onChange={this.changeFilter} /> */}
+        <Filter filter={this.state.filter} onChange={this.changeFilter} />
 
         <ContactList
           contacts={filterContacts}
